8d05b12260b1ff67d5be2a4728dd5af734d53732
Matheus Padilha Ansolin <matheusp.ansolin@gmail.com>
2023-02-16 09:58:11 -0300

    ending first part js

--- arquivo.js
+++ arquivo.js
@@ -1,10 +1,28 @@
-var a = document.getElementById('numberA')
-var b = document.getElementById('numberB')
+let elementoResultadoSoma = document.getElementById('resultadoSoma');
+let elementoResultadoRaiz = document.getElementById('resultadoRaiz');
+let elementoResultadoPorcentagem = document.getElementById('resultadoPorcentagem');
 
-function definirValores(){
+elementoResultadoSoma.innerHTML = 0;
+elementoResultadoRaiz.innerHTML = 0;
+elementoResultadoPorcentagem.innerHTML = 0;
+// funções do código
+function calcularSoma(a, b){
+    return (a+b);
+}
+const calcularRaiz = (a) => Math.sqrt(a);
+const calcularPorcentagem = (a, b) => `${((a*100)/b).toFixed(2)}%`;
+    
+//função main
+function calcular(){
+    let inputA = document.getElementById('primeiroNumero').value;
+    let inputB = document.getElementById('segundoNumero').value;
+
+    let a = parseFloat(inputA);
+    let b = parseFloat(inputB);
+
+    elementoResultadoSoma.innerHTML = calcularSoma(a,b);
+    elementoResultadoRaiz.innerHTML = calcularRaiz(a);
+    elementoResultadoPorcentagem.innerHTML = calcularPorcentagem(a,b);
 
 }
-function addEventos(){
-    document.getElementById("btn1").addEventListener("click",definirValores);
-}
-window.addEventListener("load", addEventos);
+
